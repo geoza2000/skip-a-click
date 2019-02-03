fed226835abf0f8b857178db174afdd08bcce4fd
George Zaimis <geoza2000@gmail.com>
2019-02-03 23:45:37 +0200

    dragable TabGroup

--- renderer.js
+++ renderer.js
@@ -3,25 +3,33 @@
 // All of the Node.js APIs are available in this process.
 
 const TabGroup = require("electron-tabs");
-let tabGroup = new TabGroup();
-let tab = tabGroup.addTab({
-    title: '',
-    iconURL: 'https://luna1.co/5ad265.png',
-    src: "https://crowdpolicy2.atlassian.net",
-    visible: true,
-    closable: false
+const dragula = require("dragula");
+
+
+let tabGroup = new TabGroup({
+    ready: function (tabGroup) {
+        dragula([tabGroup.tabContainer], {
+            direction: "vertical"
+        });
+    }
 });
+
 let tab1 = tabGroup.addTab({
     title: "",
     iconURL: 'https://upload.wikimedia.org/wikipedia/commons/4/4e/Gmail_Icon.png',
     src: "http://mail.crowdpolicy.com",
-    visible: true,
     closable: false
 });
+let tab = tabGroup.addTab({
+    title: '',
+    iconURL: 'https://luna1.co/5ad265.png',
+    src: "https://crowdpolicy2.atlassian.net",
+    closable: false
+});
+
 let tab2 = tabGroup.addTab({
     title: "",
     iconURL: 'https://a.slack-edge.com/45901/marketing/img/_rebrand/meta/slack_hash_256.png',
     src: "https://crowdpolicy.slack.com/",
-    visible: true,
     closable: false
 });
